ab8cfd681990cd338dc2001cc8f429b538d4ffd7
talong <513536236@qq.com>
2017-06-12 16:58:36 +0800

    添加Maven创建的基础架构

---
+++ book-email/src/main/java/com/spring_action/book/book_email/App.java
@@ -0,0 +1,13 @@
+package com.spring_action.book.book_email;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ book-perisist/src/main/java/com/spring_action/book/book_perisist/App.java
@@ -0,0 +1,13 @@
+package com.spring_action.book.book_perisist;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
