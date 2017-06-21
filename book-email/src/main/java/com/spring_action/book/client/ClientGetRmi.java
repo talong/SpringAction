07ac1d55cfd93e3fe24d742f489bda7bc1b12ba6
talong <513536236@qq.com>
2017-06-21 14:41:43 +0800

    增加HttpInvoker方式rmi

--- book-email/src/main/java/com/spring_action/book/client/ClientGetRmi.java
+++ book-email/src/main/java/com/spring_action/book/client/ClientGetRmi.java
@@ -1,6 +1,7 @@
 package com.spring_action.book.client;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.remoting.httpinvoker.HttpInvokerProxyFactoryBean;
 import org.springframework.remoting.rmi.RmiProxyFactoryBean;
 
 import com.spring_action.book.service.EmailService;
@@ -20,6 +21,23 @@ public class ClientGetRmi {
 		rmiProxy.setServiceInterface(EmailService.class);
 		
 		return rmiProxy;
+	
+	}
+	
+	
+	/**
+	 * 使用HttpInvokerProxyFactoryBean引用EmailService
+	 * 的RMI服务
+	 * @return
+	 */
+	@Bean
+	public HttpInvokerProxyFactoryBean emailServiceOfHttp() {
+		HttpInvokerProxyFactoryBean proxy = 
+				new HttpInvokerProxyFactoryBean();
+		proxy.setServiceUrl("http://localhost:8080/Spitter/email.service");  //EmailService服务名
+		proxy.setServiceInterface(EmailService.class);
+		
+		return proxy;
 	
 	}
 }
